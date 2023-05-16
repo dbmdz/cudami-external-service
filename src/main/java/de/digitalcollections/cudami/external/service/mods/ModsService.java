f5bb76b866d10c265983d960c2ea701d068074d8
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2023-05-16 15:38:43 +0200

    Add top level structure

--- src/main/java/de/digitalcollections/cudami/external/service/mods/ModsService.java
+++ src/main/java/de/digitalcollections/cudami/external/service/mods/ModsService.java
@@ -1,31 +1,118 @@
 package de.digitalcollections.cudami.external.service.mods;
 
-import de.digitalcollections.model.identifiable.Identifier;
 import de.digitalcollections.model.identifiable.entity.digitalobject.DigitalObject;
 import de.digitalcollections.model.identifiable.entity.item.Item;
 import org.mycore.libmeta.mods.model.Mods;
+import org.mycore.libmeta.mods.model._misc.enums.RelatedItemType;
+import org.mycore.libmeta.mods.model._toplevel.Identifier;
 import org.mycore.libmeta.mods.model._toplevel.Location;
+import org.mycore.libmeta.mods.model._toplevel.OriginInfo;
+import org.mycore.libmeta.mods.model._toplevel.PhysicalDescription;
+import org.mycore.libmeta.mods.model._toplevel.RelatedItem;
 import org.mycore.libmeta.mods.model.location.ShelfLocator;
 import org.springframework.stereotype.Service;
 
 /** Service for creation of METS metadata by given (fully filled) DigitalObject. */
 @Service
 public class ModsService {
-  public Mods getModsForDigitalObject(DigitalObject digitalObject) throws Exception {
-    Mods mods = new Mods();
+  protected Identifier createIdentifierPurl(DigitalObject digitalObject) {
+    // TODO
 
-    // mods:location/mods:shelfLocator
+    Identifier identifier = Identifier.builderForIdentifier().type("purl").build();
+    return identifier;
+  }
+
+  protected Identifier createIdentifierUrn(DigitalObject digitalObject) {
+    // TODO
+
+    Identifier identifier = Identifier.builderForIdentifier().type("urn").build();
+    return identifier;
+  }
+
+  protected Location createLocation(DigitalObject digitalObject) {
+    // mods:shelfLocator
+    ShelfLocator shelfLocator = createShelfLocator(digitalObject);
+
+    Location location = Location.builderForLocation().addShelfLocator(shelfLocator).build();
+    return location;
+  }
+
+  protected OriginInfo createOriginInfo(DigitalObject digitalObject) {
+    // mods:place
+    // TODO
+
+    // mods:dateIssued
+    // TODO
+
+    OriginInfo originInfo = OriginInfo.builderForOriginInfo().build();
+    return originInfo;
+  }
+
+  protected PhysicalDescription createPhysicalDescription(DigitalObject digitalObject) {
+    // TODO use DigitalOrigin.REFORMATTED_DIGITAL
+    // see https://github.com/MyCoRe-Org/libmeta/issues/5
+    // PhysicalDescription physicalDescription =
+    //   PhysicalDescription.builderForPhysicalDescription().addContent(???).build();
+    PhysicalDescription physicalDescription =
+        PhysicalDescription.builderForPhysicalDescription().build();
+    return physicalDescription;
+  }
+
+  protected RelatedItem createRelatedItem(DigitalObject digitalObject) {
+    // mods:titleInfo
+    // TODO
+
+    // mods:language
+    // TODO
+
+    // mods:recordInfo
+    // TODO
+
+    RelatedItem relatedItem =
+        RelatedItem.builderForRelatedItem().type(RelatedItemType.HOST).build();
+    return relatedItem;
+  }
+
+  protected ShelfLocator createShelfLocator(DigitalObject digitalObject) {
+    ShelfLocator shelfLocator = null;
     Item item = digitalObject.getItem();
-    Identifier shelfNo = item.getIdentifierByNamespace("shelfno");
+    de.digitalcollections.model.identifiable.Identifier shelfNo =
+        item.getIdentifierByNamespace("shelfno");
     if (shelfNo != null) {
-      ShelfLocator shelfLocator =
-          ShelfLocator.builderForShelfLocator().content(shelfNo.getId()).build();
-      Location location = Location.builderForLocation().addShelfLocator(shelfLocator).build();
-      mods.getContent().add(location);
+      shelfLocator = ShelfLocator.builderForShelfLocator().content(shelfNo.getId()).build();
     }
+    return shelfLocator;
+  }
 
-    // mods:relatedItem/mods:titleInfo/mods:title
+  public Mods getModsForDigitalObject(DigitalObject digitalObject) throws Exception {
 
+    // mods:location
+    Location location = createLocation(digitalObject);
+
+    // mods:relatedItem
+    RelatedItem relatedItem = createRelatedItem(digitalObject);
+
+    // mods:physicalDescription
+    PhysicalDescription physicalDescription = createPhysicalDescription(digitalObject);
+
+    // mods:identifier type="purl"
+    Identifier identifierPurl = createIdentifierPurl(digitalObject);
+
+    // mods:identifier type="urn"
+    Identifier identifierUrn = createIdentifierUrn(digitalObject);
+
+    // mods:originInfo
+    OriginInfo originInfo = createOriginInfo(digitalObject);
+
+    Mods mods =
+        Mods.builder()
+            .addContent(location)
+            .addContent(relatedItem)
+            .addContent(physicalDescription)
+            .addContent(identifierPurl)
+            .addContent(identifierUrn)
+            .addContent(originInfo)
+            .build();
     return mods;
   }
 }
